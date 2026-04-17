c4a383f2d4a2fbf2e5ea0dc0879692e5f7e7ceb6
Angel Yamir <innovogen375@gmail.com>
2026-04-17 10:39:07 -0600

    cmabios

--- config/config.js
+++ config/config.js
@@ -3,13 +3,26 @@ module.exports = {
   "development": {
     "use_env_variable": 'DEV_DATABASE_URL',
     "dialect": 'postgres',
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
   "test": {
-   "url": process.env.TEST_DATABASE_URL,
-   "dialect": 'postgres',
+    "use_env_variable": 'TEST_DATABASE_URL',
+    "dialect": 'postgres',
   },
   "production": {
-   "url": process.env.DATABASE_URL,
-   "dialect": 'postgres',
+    "use_env_variable": 'DATABASE_URL',
+    "dialect": 'postgres',
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
 }
+
--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const envConfigs =  require('../config/config');
+const envConfigs = require('../config/config');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
@@ -19,13 +19,10 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-  return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3)=== '.js');
-
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-   // const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
-    console.log(model);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
